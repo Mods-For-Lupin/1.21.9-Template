4a5f9faf4530c9532b20c5eadf63363e84906b25
jason13official <jason13official@gmail.com>
2025-10-05 20:02:30 -0400

    fix: add monolib to template

--- common/src/main/java/com/cursee/examplemod/Constants.java
+++ common/src/main/java/com/cursee/examplemod/Constants.java
@@ -7,5 +7,8 @@ public class Constants {
 
   public static final String MOD_ID = "examplemod";
   public static final String MOD_NAME = "ExampleMod";
+  public static final String MOD_VERSION = "1.0.0";
+  public static final String MOD_PUBLISHER = "Lupin";
+  public static final String MOD_URL = "https://www.curseforge.com/members/lupin/projects";
   public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
